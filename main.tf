0190d2e622a7232b16e19b910d915c72b5ecb792
Chris Steinmeyer <csteinmeyer@gmail.com>
2020-04-23 23:32:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "random_pet" "server" {
 }
 
 
-resource "aws_instance" "basic" {
-  ami           = "ami-62131702"
-  instance_type = "t3.nano"
-}
+#resource "aws_instance" "basic" {
+#  ami           = "ami-62131702"
+#  instance_type = "t3.nano"
+#}
